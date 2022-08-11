5f8ce9ee081570e78910ab8165cea42a023fd549
Gustavo Gontijo <gustavo.gontijo@base2.com.br>
2022-08-11 10:13:09 -0300

    Correção referente ao caminho relativo.

--- utility/jsonUtility.py
+++ utility/jsonUtility.py
@@ -2,9 +2,8 @@ import json
 
 
 class JsonUtility:
-
     def read_environment_json(field):
-        with open('C:\\MyProjects\\Mobile\\Robot_Python\\environment.json') as config_file:
+        with open('../environment.json') as config_file:
             config = json.load(config_file)
             environment = config["environment"]
             result = config[environment][field]
